1ce75205a47bf9c665e12c4b05fdcf5e941da047
elmita baidhya <elmita.baidhya@edu.turkuamk.fi>
2020-08-07 22:56:37 +0300

    trydjango commit

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #own app
     'products',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
